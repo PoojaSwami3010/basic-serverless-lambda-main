ca371b8934076788db3c5b342c5e30b2e58a6eef
Pooja Swami <pooja.s@floatinity.com>
2023-03-13 18:07:14 +0530

    added api

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-// const express=require('express')
+const express=require('express')
 // const serverless=require('serverless-http')
-// const app=express()
+const app=express()
 
 
-// app.get('/hello',(req,res)=>{
-//     res.send("hello world app lambda and github using aws")
-// });
+app.get('/hello',(req,res)=>{
+    res.send("hello world app lambda and github using aws")
+});
 
-// module.exports=serverless(app)
+module.exports=app
 
-'use strict';
+// 'use strict';
 
-console.log("Generate random number ")
-module.exports.generateRandomNumber = async (event) => {
-  const randomNumber=parseInt(Math.random()*100)
-  console.log("this random generated integer is:  ",randomNumber)
-    return randomNumber
-};
+// console.log("Generate random number ")
+// module.exports.generateRandomNumber = async (event) => {
+//   const randomNumber=parseInt(Math.random()*100)
+//   console.log("this random generated integer is:  ",randomNumber)
+//     return randomNumber
+// };
 
