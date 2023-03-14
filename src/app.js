9d7cde4077c632f3fdc5b76e178a515ae7278082
Pooja Swami <pooja.s@floatinity.com>
2023-03-14 10:52:21 +0530

    testing apis

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express=require('express')
 // const serverless=require('serverless-http')
 const app=express()
 
-
 app.get('/hello',(req,res)=>{
     res.send("hello world app lambda and github using aws")
 });
