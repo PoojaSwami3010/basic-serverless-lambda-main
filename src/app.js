e4a6f70150f4f40cc1eb94c80f48dc70ac67af1f
Pooja Swami <pooja.s@floatinity.com>
2023-04-04 16:57:20 +0530

    fixes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express=require('express');
-require('./db/mongooes');
+const mongoose = require('mongoose')
 const userRouter = require('./route/user-route')
 const KonvaRouter =require('./route/konva-route')
 const app=express()
@@ -10,7 +10,16 @@ app.use(express.json())
 app.use(userRouter);
 app.use(KonvaRouter);
 
-app.get('',(req,res)=>{
+app.get('',async(req,res)=>{
+    mongoose.set('strictQuery', false);
+    console.log("Connecting to DB")
+    try {
+        await mongoose.connect('mongodb+srv://admin:OIoiKUJ4VuuoVc28@elecdraw-dev.hebze6b.mongodb.net/?retryWrites=true&w=majority')
+        console.log(mongoose.connection.readyState,`0 = disconnected;1 = connected;2 = connecting;3 = disconnecting;99 = uninitialized`)
+    } catch (error) {
+        console.log("Failed to connect to DB",error);
+    }
+    
     res.send("hello world app lambda and github using aws and It's working fine")
 });
 
